8070a2c9681be1449d1855001fe541cbb3377488
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-07 16:12:05 -0600

    add get route for teachers.  modifying rake routes with application.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
   resources :students
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/teacher/:id', to: 'teachers#show', as: 'teacher'
 end
-
-GET(request) /students/12
-{ id: '12' }
-# going to GET the student with an id of 12
-# direct to students controller and the show action (students#show)
-# which then will direct to the views/students/show html page and display info regarding student 12
-# generate paths and URLs
