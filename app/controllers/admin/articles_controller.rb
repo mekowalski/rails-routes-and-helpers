77d8fbb3b42d86d5216217b0f8369420c62b11ed
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-08 12:08:32 -0600

    create admin nested controller folder, add articles con, comment routes.rb

---
+++ app/controllers/admin/articles_controller.rb
@@ -0,0 +1,6 @@
+class Admin::ArticlesController < ApplicationController
+end
+
+#^^^used like any other controller even with namespace
+#namepsace the controller and connect it, rails will recoginize it as the controller#action
+#and structurally go in namespace/nested folder
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # resources :students
-  namespace :admin do
-    resources :articles, :comments
-  end
+  # namespace :admin do
+  #   resources :articles, :comments
+  # end
 
-  scope module: 'admin' do
-    resources :articles
-  end
+  # scope module: 'admin' do
+  #   resources :articles
+  # end
 
   #same
   #resources :articles, module 'admin'
